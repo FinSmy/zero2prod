35930e27ec6c03299846b9fde4bf80c9fba63f8c
finlay.smyth@cambridgeconsultants.com <fas@B007732.uk.cambridgeconsultants.com>
2026-09-22 16:09:28 +0100

    Got onto logging
    Haven't yet done pg115

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use zero2prod::run;
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
-    run(listener)?.await
+    let env
+
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgers");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
